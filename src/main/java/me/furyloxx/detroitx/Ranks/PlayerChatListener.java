31b5462f86deebed53cf2ac7cf014f346f3f45bd
Furyloxx <161728619+Furyloxx@users.noreply.github.com>
2024-09-06 17:16:41 +0530

    Update PlayerChatListener.java

--- src/main/java/me/furyloxx/detroitx/Ranks/PlayerChatListener.java
+++ src/main/java/me/furyloxx/detroitx/Ranks/PlayerChatListener.java
@@ -11,9 +11,9 @@ public class PlayerChatListener implements Listener {
     @EventHandler
     public void Event(AsyncPlayerChatEvent e) {
         String userTag;
-        e.setFormat("%1$s" + (data.rank== PlayerRank.DEFAULT ? ChatColor.GRAY : ChatColor.WHITE) + ": %2$s");
+        e.setFormat("%1$s" + (rank== PlayerRank.DEFAULT ? ChatColor.GRAY : ChatColor.WHITE) + ": %2$s");
         e.getPlayer().setDisplayName(e.getPlayer().getName());
-        PlayerRank rank = data.rank;
+        PlayerRank rank = rank;
         if (rank == PlayerRank.DEFAULT) {
             userTag = rank.getPrefix() + e.getPlayer().getName();
         } else {
@@ -22,7 +22,7 @@ public class PlayerChatListener implements Listener {
         if (!e.getPlayer().getDisplayName().equals(ChatColor.translateAlternateColorCodes('&', userTag))) {
             e.getPlayer().setDisplayName(ChatColor.translateAlternateColorCodes('&', userTag));
         }
-        if (rank.isAboveOrEqual(PlayerRank.MVP)) {
+        if (rank.isAboveOrEqual(PlayerRank.MVPPLUSPLUS)) {
             e.setMessage(e.getMessage().replace("<3", "§c❤").replace("⭐", "§6✭").replace(":owo:", "§dO§5w§dO").replace("o/", "§d(/◕ヮ◕)/").replace(":OOF:", "§c§lOOF").replace(":123:", "§a1§e2§c3").replace(":shrug:", "§e¯\\(ツ)/¯").replace(":yes:", "§a✔").replace(":no:", "§c✖").replace(":java:", "§b♨").replace(":arrow:", "§e➡").replace(":typing:", "§e✎§6..."));
         }
     }
