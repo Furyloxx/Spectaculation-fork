d874425c60394c59ef33459a60468332b3048fb9
Furyloxx <furyloxx@gmail.com>
2024-10-06 19:36:22 +0530

    Florida Man

---
+++ src/main/java/me/superischroma/spectaculation/entity/zombie/FloridaMan.java
@@ -0,0 +1,58 @@
+package me.superischroma.spectaculation.entity.zombie;
+
+import me.superischroma.spectaculation.entity.*;
+import me.superischroma.spectaculation.item.SMaterial;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FloridaMan extends BaseZombie
+{
+    @Override
+    public String getEntityName()
+    {
+        return "Zombie Villager";
+    }
+
+    @Override
+    public double getEntityMaxHealth()
+    {
+        return 120000000000000000000000000.0;
+    }
+    
+    @Override
+    public double getEntityLevel() {
+      return 1000;
+    }
+
+    @Override
+    public double getDamageDealt()
+    {
+        return 0.0;
+    }
+
+    @Override
+    public double getMovementSpeed()
+    {
+        return 0.00;
+    }
+
+    public double getXPDropped()
+    {
+        return 0.0;
+    }
+
+    @Override
+    public boolean isBaby()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isVillager()
+    {
+        return false;
+    }
+}
